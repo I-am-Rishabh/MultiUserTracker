8087dd80bb7280946874ad96cff0b6019ec66f1b
RISHABH <rishabhprasad679367@gmail.com>
2025-04-18 15:49:34 +0530

    first commit

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
-const express = require('express');
+require('dotenv').config();
+
+const express = require("express");
 const app = express();
-const path = require('path');
-const http = require('http');
-const socketio = require('socket.io');
+const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
 
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public'))); // Use app.use here
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public"))); // Use app.use here
 
-io.on('connection',function (socket)  {
-    socket.on("send-location", function (data) {
-    io.emit("receive-location", {id: socket.id, ...data});
-    });
+io.on("connection", function (socket) {
+  socket.on("send-location", function (data) {
+    io.emit("receive-location", { id: socket.id, ...data });
+  });
 
-    socket.on("disconnect", function() {
-        io.emit("user-disconnected", socket.id);
-    })
+  socket.on("disconnect", function () {
+    io.emit("user-disconnected", socket.id);
+  });
 });
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
-
-server.listen(3000, () => {
-  
-    console.log('Server running on port 3000');
+const PORT = process.env.PORT || 10000;
+server.listen(PORT ,() => {
+  console.log(`Server running on port ${PORT}`);
 });
